1e91e9200a694ec7540517f83d3d677e46deab64
kzhou53 <kzhou22@amherst.edu>
2021-05-11 10:59:23 -0400

    updated threatened wrangling

--- data_wrangling_2.R
+++ data_wrangling_2.R
@@ -93,20 +93,52 @@ threatened_country <- threatened_species %>%
   group_by(Red_list_category, Country) %>%
   summarise(num_species = n())
 
+# renaming countries to join with world map
+setdiff(threatened_country$Country, world_map$region)
+
+threatened_country_clean <- threatened_country %>%
+  mutate(Country = as.character(Country)
+         , Country = case_when(Country == "Bonaire, Saint Eustatius And Saba" ~ "Bonaire"
+                               , Country == "Tanzania, United Republic Of" ~ "Tanzania"
+                               , Country == "Viet Nam" ~ "Vietnam"
+                               , Country == "Congo, The Democratic Republic Of The" ~ "Democratic Republic of the Congo"
+                               , Country == "Gibraltar" ~ "Spain"
+                               , Country == "Iran, Islamic Republic Of" ~ "Iran"
+                               , Country == "Taiwan, Province Of China" ~ "Taiwan"
+                               , Country == "Bolivia, Plurinational State Of" ~ "Bolivia"
+                               , Country == "Falkland Islands (Malvinas)" ~ "Falkland Islands"
+                               , Country == "Macedonia, The Former Yugoslav Republic Of" ~ "Macedonia"
+                               , Country == "Cura̤ao" ~ "Curacao"
+                               , Country == "Antigua And Barbuda" ~ "Antigua"
+                               , Country == "Antigua And Barbuda" ~ "Barbuda"
+                               , Country == "Korea, Republic Of" ~ "North Korea"
+                               , Country == "Korea, Republic Of" ~ "South Korea"
+                               , Country == "Trinidad and Tobago" ~ "Tobago"
+                               , Country == "Trinidad and Tobago" ~ "Trinidad"
+                               , Country == "Saint Helena, Ascension And Tristan Da Cunha" ~ "Saint Helena"
+                               , Country == "United Kingdom" ~ "UK"
+                               , Country == "Venezuela, Bolivarian Republic Of" ~ "Venezuela"
+                               , Country == "Virgin Islands, U.S." ~ "Virgin Islands"
+                               , Country == "C̫te D'Ivoire" ~ "Ivory Coast"
+                               , Country == "Hong Kong" ~ "China"
+                               , Country == "Lao People's Democratic Republic" ~ "Laos"
+                               , Country == "Syrian Arab Republic" ~ "Syria"
+                               , Country == "Heard Island And McDonald Islands" ~ "Heard Island"
+                               , Country == "Saint Kitts And Nevis" ~ "Saint Kitts"
+                               , Country == "Virgin Islands, British" ~ "Virgin Islands"
+                               , Country == "United States" ~ "USA"
+                               , Country == "Russian Federation" ~ "Russia"
+                               , TRUE ~ Country))
+
+# changing red list category name to join
+threatened_country_clean$Red_list_category <- gsub("LR.*", "LC", 
+                                                   threatened_country_clean$Red_list_category)
+
 # joining the names of red list categories
-threatened_iucn <- threatened_country %>%
+threatened_iucn <- threatened_country_clean %>%
   left_join(red_list_cats, by = "Red_list_category") %>%
   drop_na()
 
 # join with world map
 world_map <- map_data(map = "world"
                       , region = ".")
-threatened_world <- threatened_iucn %>%
-  full_join(world_map, by = c("Country" = "region")) 
-
-# ggplot(threatened_world, aes(x = long, y = lat, group = group)) +
-#   geom_polygon(fill="lightgray", colour = "white") +
-#   # remove background color and ticks
-#   theme_void()  +
-#   # make aspect ratio fixed
-#   coord_fixed(ratio = 1.3) 
